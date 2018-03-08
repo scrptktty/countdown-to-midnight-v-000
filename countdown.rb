53f094dacda4bd03817a7d25c6bd74980c1c01fe
Kachel Wittig <kjwittig@gmail.com>
2018-03-08 00:35:16 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,6 +2,16 @@ def countdown(n)
   while n > 0
     puts "#{n} SECOND(S)!"
     n -= 1
+  end
+
+  puts "HAPPY NEW YEAR!"
+
+end
+
+def countdown_with_sleep(n)
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
     sleep(1)
   end
 
